670262e1b0c7941ded7120becf000ae3efc8c78b
Sevil Yılmaz <sevilyilmaz@users.noreply.github.com>
2021-04-11 13:51:16 +0200

    feat: moves the Github link to the footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,8 +2,16 @@
   <div class="bg-dark text-light mt-4">
     <footer class="container py-3">
       <h5>Disclaimer</h5>
-      <p class="fs-6">
+      <p>
         This application does not give any financial advice. You can lose your money by investing. The calculations in this application are focused on investing in the Netherlands and might or might not be fully accurate.
+      </p>
+
+      <p>
+        The <a
+          href="https://github.com/sevilyilmaz/portfolio-comparator"
+          target="_blank"
+          class="link-light"
+        >source code</a> is on Github.
       </p>
     </footer>
   </div>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,20 +1,9 @@
 <template>
   <header class="bg-dark text-light">
     <div class="container py-3 mb-3">
-      <div class="row">
-        <div class="col-9">
-          <h4>Simple Portfolio Comparator</h4>
-        </div>
-        <div class="col-3 text-end">
-          <a
-            href="https://github.com/sevilyilmaz/portfolio-comparator"
-            target="_blank"
-            class="btn btn-light text-decoration-none"
-          >
-            View on Github
-          </a>
-        </div>
-      </div>
+      <h1 class="h4">
+        Simple Portfolio Comparator
+      </h1>
     </div>
   </header>
 </template>
