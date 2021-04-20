4a62b315e868adb357bd2d3d8d7efe56640bcd4e
Sevil Yılmaz <sevilyilmaz@users.noreply.github.com>
2021-04-20 21:49:50 +0200

    refactor: moves security calculations to a separate file

---
+++ src/composables/use-securities.spec.ts
@@ -0,0 +1,98 @@
+import { substractSecurityExitCost, addSecurityValues } from './use-securities';
+
+describe('substractSecurityExitCost', () => {
+  it('should substract exit cost for a single security', () => {
+    const securities = [
+      {
+        id: 'test-id',
+        name: 'NT World',
+        tertd: 0.2,
+        dividend: 0,
+        annualReturn: 12.66,
+        fixedTransactionCost: 0,
+        variableTransactionCost: 0,
+        exitCost: 0.1,
+        allocation: 100,
+      },
+    ];
+    expect(substractSecurityExitCost(1000, securities)).toBe(999);
+  });
+
+  it('should substract exit cost for multiple securities', () => {
+    const securities = [
+      {
+        id: 'nt-world',
+        name: 'NT World',
+        tertd: 0.2,
+        dividend: 0,
+        annualReturn: 12.66,
+        fixedTransactionCost: 0,
+        variableTransactionCost: 0,
+        exitCost: 0.1,
+        allocation: 86,
+      },
+      {
+        id: 'nt-em',
+        name: 'NT EM',
+        tertd: 0.37,
+        dividend: 0,
+        annualReturn: 11.38,
+        fixedTransactionCost: 0,
+        variableTransactionCost: 0,
+        exitCost: 0.2,
+        allocation: 14,
+      },
+    ];
+
+    expect(substractSecurityExitCost(1000, securities)).toBe(998.86);
+  });
+});
+
+describe('addSecurityValues', () => {
+  it('should add a security return and costs to the amount', () => {
+    const securities = [
+      {
+        id: 'test-id',
+        name: 'NT World',
+        tertd: 0.2,
+        dividend: 0,
+        annualReturn: 12.66,
+        fixedTransactionCost: 0,
+        variableTransactionCost: 0,
+        exitCost: 0.1,
+        allocation: 100,
+      },
+    ];
+
+    expect(addSecurityValues(1000, securities)).toBe(1124.6);
+  });
+
+  it('should add multiple security returns and costs to the amount', () => {
+    const securities = [
+      {
+        id: 'nt-world',
+        name: 'NT World',
+        tertd: 0.2,
+        dividend: 0,
+        annualReturn: 12.66,
+        fixedTransactionCost: 0,
+        variableTransactionCost: 0,
+        exitCost: 0.1,
+        allocation: 86,
+      },
+      {
+        id: 'nt-em',
+        name: 'NT EM',
+        tertd: 0.37,
+        dividend: 0,
+        annualReturn: 11.38,
+        fixedTransactionCost: 0,
+        variableTransactionCost: 0,
+        exitCost: 0.2,
+        allocation: 14,
+      },
+    ];
+
+    expect(addSecurityValues(1000, securities)).toBe(1122.57);
+  });
+});
---
+++ src/composables/use-securities.ts
@@ -0,0 +1,40 @@
+import { Security } from '../types';
+import { substractPercentage } from './use-substract-percentage';
+
+export function substractSecurityExitCost(
+  amount: number,
+  securities: Security[]
+): number {
+  const exitCost = securities.reduce((acc, curr) => {
+    acc += (curr.exitCost * curr.allocation) / 100;
+    return acc;
+  }, 0);
+
+  return substractPercentage(amount, exitCost);
+}
+
+export function addSecurityValues(
+  amount: number,
+  securities: Security[]
+): number {
+  const { variableCost, fixedCost, annualReturn } = securities.reduce(
+    (acc, curr) => {
+      acc.variableCost +=
+        ((curr.variableTransactionCost + curr.tertd) * curr.allocation) / 100;
+      acc.fixedCost += curr.fixedTransactionCost * 12; // monthly
+      acc.annualReturn += curr.annualReturn * (curr.allocation / 100);
+
+      return acc;
+    },
+    {
+      variableCost: 0,
+      fixedCost: 0,
+      annualReturn: 0,
+    }
+  );
+
+  const costs = (amount * variableCost) / 100 + fixedCost;
+  const growth = (amount * annualReturn) / 100;
+
+  return amount + growth - costs;
+}
