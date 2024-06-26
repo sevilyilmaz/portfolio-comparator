47cb2d4746aa27b4cb46ed92550418224e343b32
Sevil Yilmaz <393186+sevilyilmaz@users.noreply.github.com>
2024-06-26 21:39:41 +0300

    feat: use 2024 wealth tax calculations

--- src/composables/use-wealth-tax.spec.ts
+++ src/composables/use-wealth-tax.spec.ts
@@ -2,12 +2,11 @@ import { describe, test, expect } from 'vitest';
 import { getWealthTax, substractWealthTax } from './use-wealth-tax';
 
 describe('getWealthTax', () => {
-  test.each([28000, 50000, 80000, 100000, 120000, 950000, 1200000])(
-    'should return the wealth tax for [%s]',
-    (amount) => {
-      expect(getWealthTax(amount)).toMatchSnapshot();
-    }
-  );
+  test.each([
+    28000, 50000, 80000, 100000, 120000, 150000, 950000, 1200000, 200000,
+  ])('should return the wealth tax for [%s]', (amount) => {
+    expect(getWealthTax(amount)).toMatchSnapshot();
+  });
 });
 
 describe('substractWealthTax', () => {
--- src/composables/use-wealth-tax.ts
+++ src/composables/use-wealth-tax.ts
@@ -1,48 +1,16 @@
-// https://www.belastingdienst.nl/wps/wcm/connect/bldcontentnl/belastingdienst/prive/vermogen_en_aanmerkelijk_belang/vermogen/belasting_betalen_over_uw_vermogen/grondslag_sparen_en_beleggen/berekening-2022/
-const wealthTax = 0.31;
-const wealthTaxBrackets = [
-  {
-    amount: 50650,
-    fee: 1.818 * wealthTax,
-  },
-  {
-    amount: 101300,
-    fee: 4.366 * wealthTax,
-  },
-  {
-    amount: 962350,
-    fee: 5.53 * wealthTax,
-  },
-];
+// https://www.belastingdienst.nl/wps/wcm/connect/nl/box-3/content/nieuwe-berekening-box-3-inkomen
+const wealthTax = 36 / 100;
+const taxFreeWealth = 57000;
+const fictitiusReturnPercentage = 6.04 / 100;
 
 export function getWealthTax(amount: number): number {
-  return wealthTaxBrackets.reduce(
-    (acc, curr, idx, arr) => {
-      const next = arr[idx + 1] ?? null;
-      let val = 0;
+  if (amount <= taxFreeWealth) return 0;
+  const fictitiusReturn = amount * fictitiusReturnPercentage;
+  const taxableAmount = amount - taxFreeWealth;
+  const taxableContributionPercentage = taxableAmount / amount;
+  const fictitiusGain = fictitiusReturn * taxableContributionPercentage;
 
-      if (next) {
-        if (acc.remainder === amount) {
-          // First 50K is tax exempt
-          acc.remainder -=
-            acc.remainder < curr.amount ? acc.remainder : curr.amount;
-        }
-
-        val = amount < next.amount ? acc.remainder : next.amount - curr.amount;
-      } else {
-        val = acc.remainder;
-      }
-
-      acc.remainder -= val;
-      acc.tax += val * (curr.fee / 100);
-
-      return acc;
-    },
-    {
-      tax: 0,
-      remainder: amount,
-    }
-  ).tax;
+  return fictitiusGain * wealthTax;
 }
 
 export function substractWealthTax(
