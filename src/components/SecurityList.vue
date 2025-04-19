00f837ded10877fe6f7cc9ff0f46e4d8e41a75b9
Sevil Yilmaz <393186+sevilyilmaz@users.noreply.github.com>
2025-04-19 14:42:38 +0200

    fix: use responsive table for mobile

--- src/components/SecurityList.vue
+++ src/components/SecurityList.vue
@@ -2,86 +2,88 @@
   <h4>Securities</h4>
   <p>Change the values in the table to adjust your portfolio</p>
 
-  <table class="table table-hover">
-    <thead>
-      <tr>
-        <th>Security</th>
-        <th>TER/TD (%)</th>
-        <th>Annual return (%)</th>
-        <!-- <th>Dividend</th> -->
-        <th>Fixed transaction cost</th>
-        <th>Variable transaction cost (%)</th>
-        <th>Exit cost (%)</th>
-        <th>Allocation (%)</th>
-        <th>Actions</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="(security, index) in securities" :key="security.id">
-        <td>
-          <input v-model="security.name" type="text" class="form-control" />
-        </td>
-        <td>
-          <input
-            v-model.number="security.tertd"
-            type="text"
-            class="form-control"
-          />
-        </td>
-        <td>
-          <input
-            v-model.number="security.annualReturn"
-            type="text"
-            class="form-control"
-          />
-        </td>
-        <!-- <td>
-          <input
-            v-model.number="security.dividend"
-            type="text"
-            class="form-control"
-          >
-        </td> -->
-        <td>
-          <input
-            v-model.number="security.fixedTransactionCost"
-            type="text"
-            class="form-control"
-          />
-        </td>
-        <td>
-          <input
-            v-model.number="security.variableTransactionCost"
-            type="text"
-            class="form-control"
-          />
-        </td>
-        <td>
-          <input
-            v-model.number="security.exitCost"
-            type="text"
-            class="form-control"
-          />
-        </td>
-        <td>
-          <input
-            v-model.number="security.allocation"
-            type="text"
-            class="form-control"
-          />
-        </td>
-        <td>
-          <button
-            type="button"
-            class="btn btn-danger"
-            @click="removeSecurity(Number(index))"
-          >
-            Remove
-          </button>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="table-responsive">
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th>Security</th>
+          <th>TER/TD (%)</th>
+          <th>Annual return (%)</th>
+          <!-- <th>Dividend</th> -->
+          <th>Fixed transaction cost</th>
+          <th>Variable transaction cost (%)</th>
+          <th>Exit cost (%)</th>
+          <th>Allocation (%)</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(security, index) in securities" :key="security.id">
+          <td>
+            <input v-model="security.name" type="text" class="form-control" />
+          </td>
+          <td>
+            <input
+              v-model.number="security.tertd"
+              type="text"
+              class="form-control"
+            />
+          </td>
+          <td>
+            <input
+              v-model.number="security.annualReturn"
+              type="text"
+              class="form-control"
+            />
+          </td>
+          <!-- <td>
+            <input
+              v-model.number="security.dividend"
+              type="text"
+              class="form-control"
+            >
+          </td> -->
+          <td>
+            <input
+              v-model.number="security.fixedTransactionCost"
+              type="text"
+              class="form-control"
+            />
+          </td>
+          <td>
+            <input
+              v-model.number="security.variableTransactionCost"
+              type="text"
+              class="form-control"
+            />
+          </td>
+          <td>
+            <input
+              v-model.number="security.exitCost"
+              type="text"
+              class="form-control"
+            />
+          </td>
+          <td>
+            <input
+              v-model.number="security.allocation"
+              type="text"
+              class="form-control"
+            />
+          </td>
+          <td>
+            <button
+              type="button"
+              class="btn btn-danger"
+              @click="removeSecurity(Number(index))"
+            >
+              Remove
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script lang="ts">
